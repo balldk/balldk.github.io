94f9f9c0134d6a6a4cbf6590cb30d5c465b9dcbe
balldk <pr0f3ss0r.b@gmail.com>
2025-06-17 18:19:27 +0700

    upgrade insecure requests

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import tailwindcss from '@tailwindcss/vite'
 import { sveltekit } from '@sveltejs/kit/vite'
 import { defineConfig, loadEnv } from 'vite'
 import { enhancedImages } from '@sveltejs/enhanced-img'
-import { imagetools } from '@zerodevx/svelte-img/vite'
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, process.cwd())
